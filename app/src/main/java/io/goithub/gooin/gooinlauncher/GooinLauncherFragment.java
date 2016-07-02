7df73114cb97452bd8577eee561e431592a962b3
gooin <gooin@outlook.com>
2016-07-02 10:58:19 +0800

    获取到安装的应用列表,可以点击项目来启动应用

--- app/src/main/java/io/goithub/gooin/gooinlauncher/GooinLauncherFragment.java
+++ app/src/main/java/io/goithub/gooin/gooinlauncher/GooinLauncherFragment.java
@@ -1,6 +1,7 @@
 package io.goithub.gooin.gooinlauncher;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -11,7 +12,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -43,7 +47,87 @@ public class GooinLauncherFragment extends Fragment {
         PackageManager pm = getActivity().getPackageManager();
         List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
 
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        a.loadLabel(pm).toString(),
+                        b.loadLabel(pm).toString()
+                );
+            }
+        });
         Log.i(TAG, "Found " + activities.size() + " activities");
+
+        mRecyclerView.setAdapter(new ActivityAdapter(activities));
     }
 
+
+    private class ActivityHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        private  ResolveInfo mResolveInfo;
+        private TextView mNameTextView;
+
+
+        public ActivityHolder(View itemView) {
+            super(itemView);
+            mNameTextView = (TextView) itemView;
+            mNameTextView.setOnClickListener(this);
+
+        }
+
+        public void bindActivity(ResolveInfo resolveInfo) {
+            mResolveInfo = resolveInfo;
+            PackageManager pm = getActivity().getPackageManager();
+            String appName = mResolveInfo.loadLabel(pm).toString();
+//            此处还可以获取logo
+            mNameTextView.setText(appName);
+
+        }
+
+
+        @Override
+        public void onClick(View view) {
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+            Intent i = new Intent(Intent.ACTION_MAIN)
+                    .setClassName(activityInfo.applicationInfo.packageName, activityInfo.name);
+
+            startActivity(i);
+
+        }
+    }
+
+    private class ActivityAdapter extends RecyclerView.Adapter<ActivityHolder> {
+        private final List<ResolveInfo> mActivities;
+
+        public ActivityAdapter(List<ResolveInfo> activities) {
+            mActivities = activities;
+        }
+        @Override
+        public ActivityHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+
+            return new ActivityHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ActivityHolder holder, int position) {
+
+            ResolveInfo resolveInfo = mActivities.get(position);
+            holder.bindActivity(resolveInfo);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mActivities.size();
+        }
+    }
+
+
+
 }
+
